31bb2fb6350ce3d397cd08ce168a3f6479e189c2
vjosullivan <vjosullivan@hotmail.com>
2018-01-19 07:51:02 +0000

    Add ToDo list to main view.

--- VOSToDo/ViewController.swift
+++ VOSToDo/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Local constants and variables.
+
+    private var items = [ToDo]()
+
+    // MARK: - Outlets.
+
+    @IBOutlet weak var todoList: UITableView!
+
+    // MARK: - UIView lifecycle functions.
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        todoList.dataSource = self
+
+        for i in 1...3 {
+            items.append(ToDo("Item \(i)."))
+        }
     }
 }
 
+extension ViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = todoList.dequeueReusableCell(withIdentifier: "ToDoCell")!
+        cell.textLabel?.text = items[indexPath.row].title
+
+        return cell
+    }
+}
