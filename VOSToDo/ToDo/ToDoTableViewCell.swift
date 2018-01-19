c6bb29beda0096712ab2dea457f9da2a47bd1c74
vjosullivan <vjosullivan@hotmail.com>
2018-01-19 08:33:38 +0000

    Add a ToDoTableViewCell

---
+++ VOSToDo/ToDo/ToDoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoTableViewCell.swift
+//  VOSToDo
+//
+//  Created by Vincent O'Sullivan on 19/01/2018.
+//  Copyright © 2018 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
